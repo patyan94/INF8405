8b38a30cdd9f336be2c71ae60248378b4be8f9ab
golnoush84 <golnoosh84@gmail.com>
2016-01-28 17:21:20 -0500

    quitbutton

--- TP1_INF8405/app/src/main/java/com/example/yannd/tp1_inf8405/MainMenu.java
+++ TP1_INF8405/app/src/main/java/com/example/yannd/tp1_inf8405/MainMenu.java
@@ -2,6 +2,8 @@ package com.example.yannd.tp1_inf8405;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        // Capture our button from layout
+        Button button = (Button) findViewById(R.id.quitButton);
+        // Register the onClick listener with the implementation above
+        button.setOnClickListener(mCorkyListener);
     }
+
+    private View.OnClickListener mCorkyListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            finish();
+        }
+    };
 }
+
+
