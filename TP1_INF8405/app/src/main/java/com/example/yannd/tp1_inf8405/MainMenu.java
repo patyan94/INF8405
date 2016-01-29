a54decc8f3a3909358c1d6b264a81b3ea34b10cc
Sacha LR <s.licatese.roussel@gmail.com>
2016-01-28 21:56:57 -0500

    added activity switch from main menu to game

--- TP1_INF8405/app/src/main/java/com/example/yannd/tp1_inf8405/MainMenu.java
+++ TP1_INF8405/app/src/main/java/com/example/yannd/tp1_inf8405/MainMenu.java
@@ -1,5 +1,6 @@
 package com.example.yannd.tp1_inf8405;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,15 +12,29 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
         // Capture our button from layout
         Button button = (Button) findViewById(R.id.quitButton);
         // Register the onClick listener with the implementation above
-        button.setOnClickListener(mCorkyListener);
+        button.setOnClickListener(quitBtnListener);
+
+        button = (Button) findViewById(R.id.playButton);
+        button.setOnClickListener(startBtnListener);
     }
 
-    private View.OnClickListener mCorkyListener = new View.OnClickListener() {
+    private View.OnClickListener quitBtnListener = new View.OnClickListener() {
         public void onClick(View v) {
             finish();
+        }
+    };
+
+    private View.OnClickListener startBtnListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            //reference : http://stackoverflow.com/questions/4186021/how-to-start-new-activity-on-button-click
+
+            Intent intent = new Intent(MainMenu.this, GamingActivity.class);
+            //intent.putExtra("key", value); //Optional parameters
+            MainMenu.this.startActivity(intent);
         }
     };
 }
