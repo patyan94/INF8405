652a342f16172571eb7f0d855901f6c7365c9c91
golnoush84 <golnoosh84@gmail.com>
2016-03-17 15:35:16 -0400

    Revert "Connectivity"
    This reverts commit d7987cdac29c8e62e8b05fbcfd0229a5d918899a.

--- TP2_INF8405/app/src/main/java/com/example/yannd/tp2_inf8405/ConnectivityManager.java
+++ TP2_INF8405/app/src/main/java/com/example/yannd/tp2_inf8405/ConnectivityManager.java
@@ -1,24 +1,20 @@
 package com.example.yannd.tp2_inf8405;
 
+/**
+ * Created by yannd on 2016-02-18.
+ */
 public class ConnectivityManager {
     public boolean IsDataNetworkAvailable(){
-        boolean mobileDataEnabled;
-        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);		// Connecting to connectivity service
-        NetworkInfo networkInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);						// Getting mobile data informations
-        mobileDataEnabled = networkInfo != null && networkInfo.isConnected();				// Check mobile data is connected or not and information is not null
-        return mobileDataEnabled;
+    // TODO
+        return false;
     }
     public boolean IsWiFiNetworkAvailable(){
-        boolean wifiEnabled;
-        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);						// Getting wifi informations
-        wifiEnabled = networkInfo != null && networkInfo.isConnected();								// Check wifi is connected or not
-        return wifiEnabled;
+        // TODO
+        return false;
     }
     public boolean IsGPSNetworkAvailable(){
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);		// Connecting to GPS serivice
-        boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);				// Checking GPS is on or not
-        return isGPSEnabled;
+        // TODO
+        return false;
     }
 
 }
